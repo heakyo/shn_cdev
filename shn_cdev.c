955764ccffabcffd5423801fcf7d0067e960818b
Marvin Ma <marvin@shannon-data.com>
2017-05-04 13:52:20 +0800

    shn_cdev can be registered into the systme
    It can be found in /sys/bus/pci/drivers/shn_cdev
    
    Signed-off-by: Marvin Ma <marvin@shannon-data.com>

--- shn_cdev.c
+++ shn_cdev.c
@@ -1,15 +1,25 @@
-#include<linux/init.h>
-#include<linux/module.h>
+#include"shn_cdev.h"
+
+static const struct pci_device_id shn_cdev_ids[] = {
+	{ PCI_DEVICE(PCI_VENDOR_ID_SHANNON, PCI_DEVICE_ID_SHANNON_25A5) },
+	{0, }
+};
+
+static struct pci_driver shn_cdev_driver = {
+	.name = "shn_cdev",
+	.id_table = shn_cdev_ids,
+};
 
 static int shn_init(void)
 {
-	printk("Hello enter\n");
-	return 0;
+	printk("shannon cdev enter\n");
+	return pci_register_driver(&shn_cdev_driver);
 }
 
 static void shn_exit(void)
 {
-	printk("Hello exit\n");
+	printk("shannon cdev exit\n");
+	pci_unregister_driver(&shn_cdev_driver);
 }
 
 module_init(shn_init);
---
+++ shn_cdev.h
@@ -0,0 +1,15 @@
+#ifndef __SHN_CDEV_H__
+#define __SHN_CDEV_H__
+
+#include<linux/init.h>
+#include<linux/module.h>
+#include<linux/pci.h>
+
+/* define */
+#define PCI_VENDOR_ID_SHANNON 	0x1CB0
+#define PCI_DEVICE_ID_SHANNON_25A5 0x25a5
+
+/* struct */
+
+#endif
+
