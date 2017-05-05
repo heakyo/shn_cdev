3e79b803ccb074860a22f9e451df4dd6e0c8f741
Marvin Ma <marvin@shannon-data.com>
2017-05-06 01:13:58 +0800

    enable/disable pci dev work
    Signed-off-by: Marvin Ma <marvin@shannon-data.com>

--- shn_cdev.c
+++ shn_cdev.c
@@ -1,5 +1,29 @@
 #include"shn_cdev.h"
 
+static int shn_cdev_probe(struct pci_dev *dev, const struct pci_device_id *id)
+{
+	printk("%s\n", __func__);
+	int rc;
+
+	rc = pci_enable_device(dev);
+	if (rc)
+		goto out;
+
+	printk("probe success\n");
+	return 0;
+
+out:
+	return rc;
+}
+
+static void shn_cdev_remove(struct pci_dev *dev)
+{
+	printk("%s\n", __func__);
+
+	pci_disable_device(dev);
+	printk("remove success\n");
+}
+
 static const struct pci_device_id shn_cdev_ids[] = {
 	{ PCI_DEVICE(PCI_VENDOR_ID_SHANNON, PCI_DEVICE_ID_SHANNON_25A5) },
 	{0, }
@@ -8,17 +32,19 @@ static const struct pci_device_id shn_cdev_ids[] = {
 static struct pci_driver shn_cdev_driver = {
 	.name = "shn_cdev",
 	.id_table = shn_cdev_ids,
+	.probe = shn_cdev_probe,
+	.remove = shn_cdev_remove,
 };
 
 static int shn_init(void)
 {
-	printk("shannon cdev enter\n");
+	printk("%s\n", __func__);
 	return pci_register_driver(&shn_cdev_driver);
 }
 
 static void shn_exit(void)
 {
-	printk("shannon cdev exit\n");
+	printk("%s\n", __func__);
 	pci_unregister_driver(&shn_cdev_driver);
 }
 
--- shn_cdev.h
+++ shn_cdev.h
@@ -4,12 +4,16 @@
 #include<linux/init.h>
 #include<linux/module.h>
 #include<linux/pci.h>
+#include<linux/cdev.h>
 
 /* define */
 #define PCI_VENDOR_ID_SHANNON 	0x1CB0
 #define PCI_DEVICE_ID_SHANNON_25A5 0x25a5
 
 /* struct */
+struct shn_cdev {
+	struct cdev cdev;
+};
 
 #endif
 
