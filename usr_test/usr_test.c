56178f9b62e07e47d61fa27072aeb985ca03d290
Marvin Ma <marvin@shannon-data.com>
2017-05-14 03:24:59 +0800

    added file to test driver of shn_cdev
    Signed-off-by: Marvin Ma <marvin@shannon-data.com>

---
+++ usr_test/usr_test.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  usr_test.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  05/14/2017 03:18:18 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
