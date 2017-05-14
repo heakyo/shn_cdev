0b7d9ba4edad80b859224bd897e09cc5468ddd69
Marvin Ma <marvin@shannon-data.com>
2017-05-14 19:42:39 +0800

    added open test in the usr_test.c
    Signed-off-by: Marvin Ma <marvin@shannon-data.com>

--- usr_test/usr_test.c
+++ usr_test/usr_test.c
@@ -20,8 +20,21 @@
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<fcntl.h>
+#include<errno.h>
+
+#define FPATH "/dev/shn_cdev"
 
 int main(int argc, char *argv[])
 {
+	int fd;
+
+	fd = open(FPATH, O_RDWR);
+	if (fd == -1) {
+		perror("open file error");
+		return errno;
+	}
+
+	close(fd);
+
 	return 0;
 }
